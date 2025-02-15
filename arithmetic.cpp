03c0f9881ced82717d73f9905f54c3417ee61705
Cy_Wilson <youremail@example.com>
2025-02-16 07:22:30 +0800

    added another simple program

---
+++ arithmetic.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num1 = 10;
+    int num2 = 20;
+
+    cout << "The sum of " << num1 << " and " << num2 << " is " << num1 + num2 << endl;
+    cout << "The difference of " << num1 << " and " << num2 << " is " << num1 - num2 << endl;
+    cout << "The product of " << num1 << " and " << num2 << " is " << num1 * num2 << endl;
+    cout << "The quotient of " << num1 << " and " << num2 << " is " << num1 / num2 << endl;
+    cout << "The remainder of " << num1 << " and " << num2 << " is " << num1 % num2 << endl;
+    return 0;
+}
